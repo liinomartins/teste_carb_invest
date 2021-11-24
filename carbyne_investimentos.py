86e540645c9f4e08629a35fe686f8c2d56c0730f
liinomartins <69821617+liinomartins@users.noreply.github.com>
2021-11-24 12:52:59 -0300

    Update carbyne_investimentos.py

--- carbyne_investimentos.py
+++ carbyne_investimentos.py
@@ -3,16 +3,19 @@
 import numpy as np
 import pandas as pd 
 import seaborn as sns
-import streamlit as st
+import streamlit as st 
 import plotly.express as px
 import matplotlib.pyplot as plt
- 
+from wordcloud import WordCloud
+
+
 # Importando a base de dados a ser analisada 
 df = pd.read_excel('BaseDados ATUALIZADA.xlsx', sheet_name=1)
 df.drop(['Unnamed: 0'], axis =1, inplace = True)
 df.dropna(how = 'all', inplace = True)
 colunas = df.columns
 fundacao = df.Fundação.unique().tolist()
+df['Fundo'].astype(str)
 quantidade_fundacao = df.groupby(['Fundação']).Fundação.count().sort_values()
 
 # Função que mostra a quantidade de linhas 
@@ -25,25 +28,16 @@ image = ('carbyne.png')
 st.sidebar.image(image, use_column_width = True)
 st.sidebar.title('Menu')
 paginaselecionada = st.sidebar.selectbox('Selecione a base que deseja ter informações',['Fundos de Pensão','Base 1', 'Base 2'])
+st.sidebar.write('  ')
+st.sidebar.write('Essas informações estão restritas a equipe da Carbyne Investimentos')
 
-# Função que cria o gráfico
-#def plot_fundacao(df, fundacao):
-
-#    dados_plot = df.query('Fundação == @fundacao')
-
-#    fig, ax = plt.subplots(figsize=(8,6))
-#    ax = sns.barplot(x = 'Fundação', y = quantidade_fundacao, data = dados_plot)
-#    ax.set_title(f'Quantidade em estoque dos produtos de {fundacao}', fontsize = 16)
-#    ax.set_xlabel('Fundação', fontsize = 12)
-#    ax.tick_params(rotation = 20, axis = 'x')
-#    ax.set_ylabel('Porte', fontsize = 12)
-  
-#    return fig
+# Configurações de cada página do menu 
 if paginaselecionada == 'Fundos de Pensão':
 
     #Configuracoes página 1 - Fundos de Pensão
     st.title('Análise Exploratória de Dados')
-    st.title('- Carbyne Investimentos')
+    st.markdown("<h1 style='color:#46A1C0;'>Carbyne Investimentos</h1>", unsafe_allow_html=True)
+    #st.title('- Carbyne Investimentos')
     st.write('Análise de dados referentes aos fundos de pensão presentes na base consolidada')
     st.markdown('''
     __*Dicionário dos dados*__
@@ -60,17 +54,21 @@ if paginaselecionada == 'Fundos de Pensão':
     - Data_Base: Data que a informação foi buscada
     - Já Falamos: Notificação de debate
     ''')
-
-    mostrar_tabela = st.checkbox('Mostrar Tabela')
-    if mostrar_tabela:
+    st.write('   ')
+#    st.dataframe(df)
+    
+    #Configuração de botão para mostrar tabela e aparecer filtros 
+#    mostrar_tabela = st.checkbox('Mostrar Tabela')
+#    if mostrar_tabela:
 
             #Configuracoes do filtro da aplicacao 
-            st.markdown('Filtro para a tabela')
-            #Filtro coluna fundacao 
-            fundacoes = list(df['Fundação'].unique())
-            fundacoes.append('Fundação Banrisul')
+#            st.markdown('Filtro para a tabela')
 
-            fundacao = st.selectbox('Selecione a Fundação', options = fundacoes)
+            #Filtro coluna fundacao 
+#            fundacoes = list(df['Fundação'].unique())
+#            fundacoes.append('Fundação Banrisul')
+
+#            fundacao = st.selectbox('Selecione a Fundação', options = fundacoes)
 
             #Filtro coluna gestor 
             #st.markdown('Filtro por Gestor')
@@ -79,21 +77,100 @@ if paginaselecionada == 'Fundos de Pensão':
 
             #gestor = st.selectbox('Selecione o Gestor', options = gestores)
 
-            if fundacao != 'Fundação Banrisul':
-                df_fundacao = df.query('Fundação == @fundacao')
-                mostra_qntd_linhas(df_fundacao)
-            else:
-                mostra_qntd_linhas(df)
-     
-    #Configuração do Gráfico de quantitativo de fundações 
+#            if fundacao != 'Fundação Banrisul':
+#                df_fundacao = df.query('Fundação == @fundacao')
+#                mostra_qntd_linhas(df_fundacao)
+#            else:
+#                mostra_qntd_linhas(df)
+
+    #Configuracoes do filtro da aplicacao 
+    st.markdown('Filtro para a tabela')
+
+    #Filtro coluna fundacao 
+    fundacoes = list(df['Fundação'].unique())
+    fundacoes.append('Todas')
+    fundacao = st.selectbox('Selecione a Fundação', options = fundacoes)
+    #Filtro coluna gestor
+    #gestores= list(df['Gestor'].unique()):
+    #gestores.append('Todos')
+    #gestor = st.selectbox('Selecione um Gestor', options = gestores)
+
+    if fundacao == ' ':
+        df = df.query('Fundação == @fundacao')
+        mostra_qntd_linhas(df)
+    else:
+        mostra_qntd_linhas(df)
+
+    #Filtro coluna gestor
+    #gestores= list(df['Gestor'].unique())
+    #gestores.append('Todas')
+    #gestor = st.selectbox('Selecione um Gestor', options = gestores)
+    #if gestor != 'Todas':
+    #    df = df.query('Gestor == @gestor')
+    #    mostra_qntd_linhas(df)
+    #else:
+     #   mostra_qntd_linhas(df)
+
+    # Visualização Gráfica
     st.title('Visualização Gráfica')
     quantidade_fundacao = df.groupby(['Fundação']).Fundação.count().sort_values()
     fundacoes = df['Fundação'].unique()
+
+    # Gráfico de Barras Horizontal 
     fig = px.bar(x = quantidade_fundacao,
                 y = fundacoes,
                 orientation='h', title="Quantitativo de Fundações",
-                labels={'x':'Quantidade de Fundações','y':'Fundação'})
+                labels={'x':'Quantidade','y':'Fundação'})
     st.plotly_chart(fig)
+    
+    #Gráfico de setores 
+    quantidade_classificacao = df.groupby(['Classificação']).Classificação.count().sort_values()
+    classificacao = df['Classificação'].unique()
+    fig1 = px.pie(df, values= quantidade_classificacao, names=classificacao, 
+                title='Representação da Classficação por Fundo') 
+    st.plotly_chart(fig1)
+
+    #Configurações do wordcloud
+    stop_words = ['em','sao','ao','de','da','do','para','c','kg','un','ml',
+                  'pct','und','das','no','ou','pc','gr','pt','cm','vd','com',
+                  'sem','gfa','jg','la','1','2','3','4','5','6','7','8','9',
+                  '0','a','b','c','d','e','lt','f','g','h','i','j','k','l',
+                  'm','n','o','p','q','r','s','t','u','v','x','w','y','z']
+    st.write('Word Cloud dos Fundos da Base')
+    if st.checkbox('Mostrar WordCloud'):
+        all_words = ','.join(str(s) for s in df['Fundo'].values)
+        # criar uma wordcloud
+        wc = WordCloud(stopwords=stop_words, background_color="white", width=1600, height=800)
+        wordcloud = wc.generate(all_words)
+        # plotar wordcloud
+        fig, ax = plt.subplots(figsize=(10,6))
+        ax.imshow(wordcloud, interpolation='bilinear')
+        ax.set_axis_off()
+        st.pyplot(fig,ax)
+
+    #Criar uma wordcloud
+#    st.write('Word Cloud dos Fundos da Base')
+#    wc = WordCloud(stopwords = stop_words, background_color="white", width=1600, height=800)
+#    wordcloud = wc.generate(all_words)
+#    #Plotar wordcloud
+#    fig, ax = plt.subplots(figsize=(10,6))
+#    ax.imshow(wordcloud, interpolation='bilinear')
+#    ax.set_axis_off()
+#    st.pyplot(fig,ax)
+
+    
+    # Valores do gráfico de Fundos 
+#   quantidade_fundos = df.groupby(['Fundação']).Fundo.count().sort_values()
+#  fig1 = px.bar(df, height=600, width=1200)
+
+#    fig1.update_layout(title='Taxa mensal de notificações por 100 mil hab.', 
+#                   xaxis_title='Mês',
+#                   yaxis_title='Casos/100 mil hab.',
+#                   barmode='group')
+
+#    st.plotly_chart(fig1)	
+
+
 
 elif paginaselecionada == 'Base 1':     
     st.title('Base 1')
@@ -124,21 +201,3 @@ elif paginaselecionada == 'Base 2':
 #fundacao_grafico = st.sidebar.selectbox('Selecione a categoria para apresentar no gráfico', options = df['Fundação'].unique())
 #figura = plot_fundacao(df, fundacao_grafico)
 #st.pyplot(figura)
-
-        
-
-
-#if paginaselecionada == 'Fundos de Pensão':
-#    st.write('Fundos de Pensão - Consolidado')
-#    st.write('Análise de dados referentes aos fundos de pensão presentes na base consolidada')
-#    st.selectbox('Selecione a Fundação', [fundacao])
-#elif paginaselecionada == 'Base 1':
-#    st.title('Análise de Dados - Base 1')
-#    x = st.slider('x') #slider interativo, bom para data science 
-#    str.write(x,'squared is', x * x)
-#elif paginaselecionada == 'Base2':
-#    st.title('Análise de Dados - Base 2')
-
-
-
-
